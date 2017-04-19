88e1357ccc2c619f4b1114d1bbc59b4451bf6d5c
namgivu <namgivu@gmail.com>
2017-04-19 12:49:04 +0700

    base layout @ template

--- app/controller/app_controller.py
+++ app/controller/app_controller.py
@@ -3,4 +3,11 @@ from app import app
 
 @app.route('/')
 def app_index():
-  return 'flask-aosh4'
+  from flask import render_template
+  return render_template('layout/base.html')
+
+
+@app.before_request
+def before_request_handler():
+  from flask import g
+  g.pageTitle = app.config['APP_NAME']
