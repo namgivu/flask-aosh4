ef2df8866876332e0024062b32e8ede03528e431
namgivu <namgivu@gmail.com>
2017-04-20 20:53:09 +0700

    simplify code

--- app/controller/statistic_controller.py
+++ app/controller/statistic_controller.py
@@ -3,15 +3,11 @@ from app import *
 
 @app.route('/statistic/leading-board')
 def statistic_leading_board():
-  return render_template(
-    'statistic/leading-board.html',
-    viewFile=userRole_to_viewFile(),
-  )
+  viewFile=userRole_to_viewFile()
+  return render_template('statistic/leading-board.html', **locals() )
 
 
 @app.route('/statistic/mission-board')
 def statistic_mission_board():
-  return render_template(
-    'statistic/mission-board.html',
-    viewFile=userRole_to_viewFile(),
-  )
+  viewFile=userRole_to_viewFile()
+  return render_template('statistic/mission-board.html', **locals() )
