3872321aca2d395da33226d0d95590a0e96c5ea3
Hoai Ngo <hoai@autonomous.nyc>
2017-04-20 19:48:10 +0700

    add player boards

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,4 +1,5 @@
 from app import *
+import random
 
 
 @app.route('/user/switch-role')
@@ -11,9 +12,51 @@ def user_dashboard_player():
   return render_template('user/dashboard-player.html')
 
 
-@app.route('/user/my-task')
-def user_my_task():
-  return render_template('user/base_task_board.html')
+@app.route('/user/player')
+def user_player_my_board():
+  days = range(1, 30)
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  names = ['Anonymous']
+  chars = "abcdefghjiklmnopqrstw"
+  for i in range(5):
+    names.append(''.join(random.sample(chars,len(chars))))
+  points = range(100, 1000)
+  fake_data = []
+  for i in range(5):
+    data = {
+      'day': random.choice(days),
+      'month': random.choice(months),
+      'point': random.choice(points),
+      'name': random.choice(names)[:9]
+    }
+    if random.randint(1, 2) == 2:
+      data['is_my_task'] = 1
+    else:
+      data['is_received_task'] = 1
+    fake_data.append(data)
+  return render_template('user/base_task_board.html', **locals())
+
+
+@app.route('/user/player/mission-board')
+def user_player_mission_board():
+  days = range(1, 30)
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  names = ['Anonymous']
+  chars = "abcdefghjiklmnopqrstw"
+  for i in range(5):
+    names.append(''.join(random.sample(chars,len(chars))))
+  points = range(100, 1000)
+  fake_data = []
+  for i in range(5):
+    data = {
+      'day': random.choice(days),
+      'month': random.choice(months),
+      'point': random.choice(points),
+      'name': random.choice(names)[:9],
+      'is_other_task': 1
+    }
+    fake_data.append(data)
+  return render_template('user/base_task_board.html', **locals())
 
 
 @app.route('/user/dashboard-giver')
