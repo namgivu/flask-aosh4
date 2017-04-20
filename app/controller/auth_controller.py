05fcdee29b529bebb000d159b7f3b0b32a9b3ede
Hoai Ngo <hoai@autonomous.nyc>
2017-04-20 12:07:29 +0700

    add login actions

--- app/controller/auth_controller.py
+++ app/controller/auth_controller.py
@@ -1,13 +1,17 @@
 from app import app
+from flask import request, redirect, url_for, render_template, session
 
 
-@app.route('/auth/sign-in')
+@app.route('/auth/sign-in', methods=["GET", "POST"])
 def auth_sign_in():
-  from flask import render_template
+  if 'login' in session or 'email' in request.form:
+    session['login'] = 1
+    return redirect(url_for('user_switch_role'))
   return render_template('auth/sign-in.html')
 
 
 @app.route('/auth/sign-out')
 def auth_sign_out():
-  from flask import render_template
-  return render_template('auth/sign-out.html')
+  if 'login' in session:
+    del session['login']
+  return redirect(url_for('app_index'))
--- application.py
+++ application.py
@@ -2,4 +2,5 @@ from app import app as application
 
 application.debug=True
 if __name__ == '__main__':
+  application.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
   application.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
