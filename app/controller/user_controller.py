2c6c4f3b72981be592f74ef90f80d33ec047fd8c
Hoai Ngo <hoai@autonomous.nyc>
2017-04-20 17:13:49 +0700

    add list task

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -10,3 +10,6 @@ def user_switch_role():
 def user_dashboard():
   return render_template('user/dashboard.html')
 
+@app.route('/user/my-task')
+def user_my_task():
+  return render_template('user/base_task_board.html')
