c87482959d641e516e29652f7927fb6f657b59c9
namgivu <namgivu@gmail.com>
2017-04-20 12:14:51 +0700

    dashboard 0th

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -5,3 +5,8 @@ from app import *
 def user_switch_role():
   return render_template('user/switch-role.html')
 
+
+@app.route('/user/dashboard')
+def user_dashboard():
+  return render_template('user/dashboard.html')
+
