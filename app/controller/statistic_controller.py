3eb69dee544b10b45a2b02be28d4cdcf536b86c9
namgivu <namgivu@gmail.com>
2017-04-21 11:18:50 +0700

    all on-going view

--- app/controller/statistic_controller.py
+++ app/controller/statistic_controller.py
@@ -3,11 +3,18 @@ from app import *
 
 @app.route('/statistic/leading-board')
 def statistic_leading_board():
-  viewFile=userRole_to_viewFile()
+  template=userRole_to_dashboardView()
   return render_template('statistic/leading-board.html', **locals() )
 
 
 @app.route('/statistic/mission-board')
 def statistic_mission_board():
-  viewFile=userRole_to_viewFile()
+  template=userRole_to_dashboardView()
   return render_template('statistic/mission-board.html', **locals() )
+
+
+@app.route('/statistic/all-on-going')
+def statistic_all_on_going():
+  template=userRole_to_dashboardView()
+  userRole = currentUserRole()
+  return render_template('statistic/all-on-going.html', **locals() )
